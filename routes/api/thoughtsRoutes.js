07e807ed036e9c8092e2798724c04e8981c66ad3
ejacosta86 <ejacosta86@gmail.com>
2024-08-17 17:50:56 -0400

    routes and server.js

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {
+    getThought,
+    addThought,
+    getThoughts,
+    deleteThought,
+    getReactions,
+    addReaction,
+    deleteReaction,
+    updateThought,
+} = require("../../controllers/thoughtsController");
+
+
+router.route("/").get(getThoughts).post(addThought);
+router.route("/:id").get(getThought).delete(deleteThought).put(updateThought);
+router.route("/:id/reactions").get(getReactions).post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const{
+    getUsers,
+    addUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+    getFriends,
+    getThoughts,
+} = require("../../controllers/usersController");
+
+router.route("/").get(getUsers).post(addUsers);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+router.route("/:id/friends").get(getFriends);
+router.route("/:id/thoughts").get(getThoughts);
+
+module.exports = router;
